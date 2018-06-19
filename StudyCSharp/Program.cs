b9b3954f7b8aa6912bd0f01c94d9799c3c21daec
zintaho@gmail.com <zintaho@gmail.com>
2018-06-19 20:17:36 +0900

    Nullable, readonly

--- StudyCSharp/Program.cs
+++ StudyCSharp/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿/*const는 컴파일시에 값이 결정되는 상수 (필드 선언시에만 초기화 가능
+ * readonly는 런타임시에 값이 결정되는 상수(필드 선언시나,  클래스 생성자에서 대입)
+ */
+using System;
 namespace StudyCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ITester tester = new TestConstants();
+            ITester tester2 = new TestConstants(75);
+            tester.Test();
+            tester2.Test();
+
+            return;
         }
     }
 }
